6796e38c9f6822d06d9b270bdf2ef1c61a346dfc
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-12-07 11:46:43 +0600

    Настройка роут-путей для перехода по компонентам

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamesComponent } from './games/games.component';
+import { ChoosePlatformComponent } from './games/choose-platform.component';
+import { PlatformGamesListComponent } from './games/platform-games-list/platform-games-list.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
+import { NewGameComponent } from './new-game/new-game.component';
 
 const routes: Routes = [
-
-];
+  {path: 'games', component: GamesComponent, children: [
+      {path: '', component: ChoosePlatformComponent},
+      {path: ':platform', component: PlatformGamesListComponent},
+    ]},
+  {path: 'new-game', component: NewGameComponent},
+  {path: 'game-details', component: GameDetailsComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
